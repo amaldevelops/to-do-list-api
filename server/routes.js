ec11fc7744ef2c57cc38c64f00cd3e2acf8888d7
Amal K <12756149+amaldevelops@users.noreply.github.com>
2025-11-10 14:30:48 +1100

    Update comments

--- server/routes.js
+++ server/routes.js
@@ -11,12 +11,12 @@ const toDoListRouter = Router();
 
 toDoListRouter.get("/", controllerMainRoute); // Main route
 
-toDoListRouter.get("/tasks", controllerGetAllTasks); //
+toDoListRouter.get("/tasks", controllerGetAllTasks); // GET /tasks Returns a list of tasks.
 
-toDoListRouter.post("/tasks", controllerPostTasks); //
+toDoListRouter.post("/tasks", controllerPostTasks); // POST /tasks Adds a new task.
 
-toDoListRouter.put("/tasks/:id", controllerUpdateTasks); //
+toDoListRouter.put("/tasks/:id", controllerUpdateTasks); // PUT /tasks/:id Updates a task (e.g., mark as completed)
 
-toDoListRouter.delete("/tasks/:id", controllerDeleteTasks); //
+toDoListRouter.delete("/tasks/:id", controllerDeleteTasks); //DELETE /tasks/:id Deletes a task.
 
 export default toDoListRouter;
